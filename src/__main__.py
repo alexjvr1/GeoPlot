778db656b013246b277e71562fafcd9b24384d38
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2024-04-17 15:10:40 +0100

    TOFIX: Can't import modules from my python scripts

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,35 @@
 ##GeoPlot##
 
+#Add the path to the root of the project to sys.path
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+print(sys.path)
+sys.path.append(str("."))
+print(sys.path)
+
+#Import modules and initiate instances of each class
+import src.importmap
+print(f"Name: {__name__}")
+print(f"Package: {__package__}")
+
+#ImportMap = importmap.ImportMap()
+
+
+#path = input("path to raster files")
+
+#Import raster file and create mosaic 
+#importmap.import_and_merge_raster_file(path)
+
 ##Plot samples on topographic maps
+
+
+
+
+#def execute_main():
+ #   if __name__ == "__main__":  # Condition to ensure module is executed not imported. 
+ #       execute_main()
+
+
+
